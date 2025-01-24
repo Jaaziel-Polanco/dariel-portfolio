102729135da503d10b34440790ac77f977858538
Jaaziel-Polanco <143868677+Jaaziel-Polanco@users.noreply.github.com>
2025-01-24 06:45:51 -0400

    fixed

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ["**/*.JPG"], // Permite incluir imágenes JPG
+  assetsInclude: ["**/*.jpg"], // Permite incluir imágenes JPG
   assetsInclude: ["**/*.png"], // Asegúrate de incluir archivos PNG como activos
   assetsInclude: ["**/*.PNG"], // Asegúrate de incluir archivos PNG como activos
 });
