d2457b2df39d4d7774ce8a7160c3e72a617f40ca
Jaaziel-Polanco <143868677+Jaaziel-Polanco@users.noreply.github.com>
2025-01-24 06:36:40 -0400

    fixed

--- src/data/data.js
+++ src/data/data.js
@@ -1,21 +1,21 @@
 export const data = [
   {
     id: 1,
-    img: "/src/assets/af520cdb-34aa-474f-aea0-86dcde583b11.JPG",
+    img: "/public/assets/af520cdb-34aa-474f-aea0-86dcde583b11.JPG",
     title: "Reporte Joyeria DPV",
     desc: "Nuestro dashboard es una herramienta esencial para comprender el desempeño financiero de DPV. Proporciona una visión clara y actualizada de las métricas clave, permitiendo a los tomadores de decisiones identificar tendencias, oportunidades y áreas de mejora.",
     link: "https://app.powerbi.com/groups/62f2a3bd-1295-4627-b495-e941b419ef9a/reports/1b79776a-3ed5-4a2d-8512-73f7600f46b4/952419f2488727bd4045?experience=power-bi",
   },
   {
     id: 2,
-    img: "/src/assets/802653a0-cf00-46ab-ac4f-5c559824fe70.JPG",
+    img: "/public/assets/802653a0-cf00-46ab-ac4f-5c559824fe70.JPG",
     title: "Analisis de Ventas",
     desc: "Este reporte ofrece una vista amplia sobre la utilidad y margen de una empresa que ha vendido sus productos en distintos paises por el mundo..",
     link: "https://app.powerbi.com/view?r=eyJrIjoiNGJjNDIzYzUtZWVjNy00YTQzLTgxN2QtOTkwNGU2MTZkMzBlIiwidCI6IjMwOTE4NjllLTFiNWMtNDlhNy1iZWQwLTA1ODJiMjBlYzg0NSIsImMiOjJ9",
   },
   {
     id: 3,
-    img: "/src/assets/fac40c59-2255-48e7-b3e3-19ba39a26ed1.JPG",
+    img: "/public/assets/fac40c59-2255-48e7-b3e3-19ba39a26ed1.JPG",
     title: "Reporte de Ventas Tecnologicas",
     desc: "Dashboard de ventas de productos tecnologicos, donde se visualiza compradores de varios sectores y de varios paises, los beneficios obtenidos por mes y gastos de los mismos con segmentadores con condiciones y graficos interactivos para mejor vista a los detalles.",
     link: "https://app.powerbi.com/groups/167169bb-d19c-4844-bbcb-4629c76e31d7/reports/bee3bed7-6106-4880-8296-48321a2c75b8/ReportSection?experience=power-bi",
